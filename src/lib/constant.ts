875138e7e0a9a984b4992000d2fde0fd95edc358
Kavishcan <kavishcan2002@gmail.com>
2025-01-01 00:29:28 +0530

    Add usage examples for button components in showcase

---
+++ src/lib/constant.ts
@@ -0,0 +1,112 @@
+export const standardButtonUsage = `<Button>Button</Button>`;
+
+export const dottedButtonUsage = `const DottedButton = () => (
+  <button className="rounded-2xl border-2 border-dashed border-black bg-white px-6 py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none">
+    Hover me
+  </button>
+);
+
+export default DottedButton;`;
+
+export const neuButtonUsage = `const NeuButton = () => {
+  return (
+    <div className="bg-white flex items-center justify-center">
+      <button className="px-6 py-2 font-medium bg-indigo-500 text-white w-fit transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
+        Hover me
+      </button>
+    </div>
+  );
+};
+
+export default NeuButton;`;
+
+export const drawOutlineButtonUsage = `const DrawOutlineButton = ({
+  children,
+  ...rest
+}: React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>) => {
+  return (
+    <button
+      {...rest}
+      className="group relative px-4 py-2 font-medium text-slate-100 transition-colors duration-[400ms] hover:text-indigo-300"
+    >
+      <span>{children}</span>
+
+      {/* TOP */}
+      <span className="absolute left-0 top-0 h-[2px] w-0 bg-indigo-300 transition-all duration-100 group-hover:w-full" />
+
+      {/* RIGHT */}
+      <span className="absolute right-0 top-0 h-0 w-[2px] bg-indigo-300 transition-all delay-100 duration-100 group-hover:h-full" />
+
+      {/* BOTTOM */}
+      <span className="absolute bottom-0 right-0 h-[2px] w-0 bg-indigo-300 transition-all delay-200 duration-100 group-hover:w-full" />
+
+      {/* LEFT */}
+      <span className="absolute bottom-0 left-0 h-0 w-[2px] bg-indigo-300 transition-all delay-300 duration-100 group-hover:h-full" />
+    </button>
+  );
+};
+
+export default DrawOutlineButton;`;
+
+export const spotlightButtonUsage = `import { motion } from "framer-motion";
+import { useEffect, useRef } from "react";
+
+const ButtonWrapper = () => {
+  return (
+    <div className="flex min-h-[200px] items-center justify-center bg-slate-800 px-4">
+      <SpotlightButton />
+    </div>
+  );
+};
+
+const SpotlightButton = () => {
+  const btnRef = useRef<HTMLButtonElement | null>(null);
+  const spanRef = useRef<HTMLSpanElement | null>(null);
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const { width } = (e.target as HTMLElement)?.getBoundingClientRect();
+      const offset = e.offsetX;
+      const left = \`\${(offset / width) * 100}%\`;
+
+      spanRef.current!.animate({ left }, { duration: 250, fill: "forwards" });
+    };
+
+    const handleMouseLeave = () => {
+      spanRef.current!.animate(
+        { left: "50%" },
+        { duration: 100, fill: "forwards" }
+      );
+    };
+
+    btnRef?.current?.addEventListener("mousemove", handleMouseMove);
+    btnRef?.current?.addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      btnRef?.current?.removeEventListener("mousemove", handleMouseMove);
+      btnRef?.current?.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
+
+  return (
+    <motion.button
+      whileTap={{ scale: 0.985 }}
+      ref={btnRef}
+      className="relative w-full max-w-xs overflow-hidden rounded-lg bg-slate-950 px-4 py-3 text-lg font-medium text-white"
+    >
+      <span className="pointer-events-none relative z-10 mix-blend-difference">
+        Hover me
+      </span>
+      <span
+        ref={spanRef}
+        className="pointer-events-none absolute left-[50%] top-[50%] h-32 w-32 -translate-x-[50%] -translate-y-[50%] rounded-full bg-slate-100"
+      />
+    </motion.button>
+  );
+};
+
+export default ButtonWrapper;
+`;
